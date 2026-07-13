14403becf5463557255e9502c2d8d80f3a255647
Lorenzo Perassi <perassi.lorenzo1804@gmail.com>
2026-07-13 15:03:06 +0200

    Update assembly information and cache files for progetto1 to progetto9; adjust informational version and cache hashes.

--- progetto3/obj/Debug/net10.0-windows/progetto3.AssemblyInfo.cs
+++ progetto3/obj/Debug/net10.0-windows/progetto3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("progetto3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9cb9ee7f92fd1a0b96489f2623cccb8f183377")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6df2653458063f1d843608729c4b409cad4270b")]
 [assembly: System.Reflection.AssemblyProductAttribute("progetto3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("progetto3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
